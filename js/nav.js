d666cdf8dae8ab061c5caf41710b7d563eeeb086
Chirag Shankar <chishankar@gmail.com>
2019-04-18 17:45:46 -0400

    make it cleaner

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+$(document).ready(
+    $("#nav").html(`
+    <a class="nav-item nav-link active" href="#"><i class="fas fa-home"></i> Home</a>
+    <a class="nav-item nav-link" href="#"><i class="fas fa-building"></i> Room Availability</a>
+    <a class="nav-item nav-link" href="#"><i class="fas fa-address-book"></i> Professor Directory</a>
+    <a class="nav-item nav-link" href="#"><i class="fas fa-map-marker-alt"></i> Map</a>
+    <a class="nav-item nav-link" href="#"><i class="fas fa-comment-alt"></i> Feedback</a>
+    `)
+)
