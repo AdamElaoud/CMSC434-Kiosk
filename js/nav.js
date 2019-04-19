4c4f883da4c4caa218f2800db0e1fbbf44c8762a
unknown <andrew.g.bian@gmail.com>
2019-04-18 20:42:59 -0400

    first commit

--- js/nav.js
+++ js/nav.js
@@ -3,17 +3,28 @@ $(document).ready(
     <a class="nav-item nav-link active" href="index.html"><i class="fas fa-home"></i> Home</a>
     <a class="nav-item nav-link" href="#"><i class="fas fa-building"></i> Room Availability</a>
     <a class="nav-item nav-link" href="#"><i class="fas fa-address-book"></i> Professor Directory</a>
-    <a class="nav-item nav-link" href="#"><i class="fas fa-map-marker-alt"></i> Map</a>
+    <a class="nav-item nav-link" href="map.html"><i class="fas fa-map-marker-alt"></i> Map</a>
     <a class="nav-item nav-link" href="feedback.html"><i class="fas fa-comment-alt"></i> Feedback</a>
     `)
 );
+
+$(document).ready(
+    $("#nav-map").html(`
+    <a class="nav-item nav-link" href="index.html"><i class="fas fa-home"></i> Home</a>
+    <a class="nav-item nav-link" href="#"><i class="fas fa-building"></i> Room Availability</a>
+    <a class="nav-item nav-link" href="#"><i class="fas fa-address-book"></i> Professor Directory</a>
+    <a class="nav-item nav-link active" href="map.html"><i class="fas fa-map-marker-alt"></i> Map</a>
+    <a class="nav-item nav-link" href="feedback.html"><i class="fas fa-comment-alt"></i> Feedback</a>
+    `)
+);
+
 
 $(document).ready(
     $("#nav-feedback").html(`
     <a class="nav-item nav-link" href="index.html"><i class="fas fa-home"></i> Home</a>
     <a class="nav-item nav-link" href="#"><i class="fas fa-building"></i> Room Availability</a>
     <a class="nav-item nav-link" href="#"><i class="fas fa-address-book"></i> Professor Directory</a>
-    <a class="nav-item nav-link" href="#"><i class="fas fa-map-marker-alt"></i> Map</a>
+    <a class="nav-item nav-link" href="map.html"><i class="fas fa-map-marker-alt"></i> Map</a>
     <a class="nav-item nav-link active" href="feedback.html"><i class="fas fa-comment-alt"></i> Feedback</a>
     `)
 );
