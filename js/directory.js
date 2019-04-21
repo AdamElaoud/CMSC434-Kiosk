91636365651e68e9df229c74774fe4bbf841ce07
Adam Elaoud <elaoud.adam@gmail.com>
2019-04-21 12:57:03 -0400

    more formatting

--- js/directory.js
+++ js/directory.js
@@ -1,4 +1,3 @@
-
 function arrayToTable(tableData) {
     var table = $('<table id="directory-table" data-toggle="table" data-sort-name="FirstName" class="table table-striped table-bordered table-sm"><thead><tr><th data-field="Room" data-sortable="true"> Room </th><th data-field="DirectoryID" data-sortable="true"> Directory ID </th><th data-field="FirstName" data-sortable="true"> First Name </th><th data-field="LastName" data-sortable="true"> Last Name </th></tr></thead></table>');
     var tbody = $('<tbody></tbody>')
