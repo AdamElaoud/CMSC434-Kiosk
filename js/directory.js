e453e1cfb827ca2c364d3030ef03efcf3d6fbc2b
Adam Elaoud <elaoud.adam@gmail.com>
2019-04-21 16:22:06 -0400

    complete overhaul of professor directory parse system

--- js/directory.js
+++ js/directory.js
@@ -1,25 +1,23 @@
 function arrayToTable(tableData) {
-    var table = $('<table id="directory-table" data-toggle="table" data-sort-name="FirstName" class="table table-striped table-bordered table-sm"><thead><tr><th data-field="Room" data-sortable="true"> Room </th><th data-field="DirectoryID" data-sortable="true"> Directory ID </th><th data-field="FirstName" data-sortable="true"> First Name </th><th data-field="LastName" data-sortable="true"> Last Name </th></tr></thead></table>');
-    var tbody = $('<tbody></tbody>')
+    let table = $('<table id="directory-table" data-toggle="table" data-sort-name="FirstName" class="table table-striped table-bordered table-sm"><thead><tr><th data-field="Room" data-sortable="true"> Room </th><th data-field="DirectoryID" data-sortable="true"> Directory ID </th><th data-field="FirstName" data-sortable="true"> First Name </th><th data-field="LastName" data-sortable="true"> Last Name </th></tr></thead></table>');
+    let tbody = $('<tbody></tbody>');
+
     $(tableData).each(function (i, rowData) {
-        var row = $('<tr></tr>');
-        $(rowData).each(function (j, cellData) {
-            if(j == 0) {
-                row.append($('<td>IRB '+cellData+'</td>'));
-            } else {
-                row.append($('<td>'+cellData+'</td>'));
-            }
-        });
+        let row = $('<tr></tr>');
+
+        row.append($('<td>IRB '+ rowData.Room +'</td>'));
+        row.append($('<td>'+ rowData.ID +'</td>'));
+        row.append($('<td>'+ rowData.First +'</td>'));
+        row.append($('<td>'+ rowData.Last +'</td>'));
+
         tbody.append(row);
     });
+
     table.append(tbody);
     return table;
 }
 
-$.ajax({
-    type: "GET",
-    url: "/data/professors.csv",
-    success: function (data) {
-        $('#container').append(arrayToTable(Papa.parse(data).data));
-    }
-});
+let data = jsonParse();
+console.log(data);
+
+$('#container').append(arrayToTable(data));
