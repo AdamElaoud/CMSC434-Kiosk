767c2d328f74959ad3d3584831f41487e7af8fbb
Adam Elaoud <elaoud.adam@gmail.com>
2019-04-22 18:59:03 -0400

    updated loading icon for events page (moved to side)

--- js/events.js
+++ js/events.js
@@ -7,14 +7,14 @@ $(".available").on('click',function(event){
     event.preventDefault();
     $("#notification").empty();
 
-    $(this).append(
+    $(this).siblings(".spinner").append(
         `<div class="spinner-border text-primary" id="loading" role="status">
             <span class="sr-only">Loading...</span>
         </div>`
     )
     setTimeout(function(){
         if ($('.emailEvent').val() != ""){
-            $("#notification").append(`  
+            $("#notification").append(`
             <div class="alert alert-success alert-dismissible ">
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                 <strong>Success!</strong> Event has been sent to your calendar at ${$('.emailEvent').val()}.
@@ -22,7 +22,7 @@ $(".available").on('click',function(event){
             removeLoading();
 
         } else{
-            $("#notification").append(`  
+            $("#notification").append(`
             <div class="alert alert-danger alert-dismissible">
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                 <strong>Error!</strong> Please fill in your email.
