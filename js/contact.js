dacc2a28b82e4117d4e1da9a93f58d98e9c63fd5
Chirag Shankar <chishankar@gmail.com>
2019-04-22 14:28:32 -0400

    added loading spinner to request, indicated request is initiated and something is happening

--- js/contact.js
+++ js/contact.js
@@ -3,22 +3,45 @@ $('#sendMessageButton').on('click',function(event){
 
     $("#notification").empty()
 
-    if ($("#name").val() != "" && $("#email").val() != "" && $("#message").val() != "" ){
-        $("#notification").append(`  
-        <div class="alert alert-success alert-dismissible ">
-            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            <strong>Success!</strong> Feedback as been submitted
-        </div>`);
+    $("#sendMessageButton").append(`
+    <div class="spinner-border text-primary" id="loading" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>`);
 
-        $("#name").val('');
-        $("#email").val('');
-        $("#message").val('');
+
+    if ($("#name").val() != "" && $("#email").val() != "" && $("#message").val() != "" ){
+
+        setTimeout(function(){
+            $("#notification").append(`  
+            <div class="alert alert-success alert-dismissible ">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Success!</strong> Feedback as been submitted
+            </div>`);
+    
+            $("#name").val('');
+            $("#email").val('');
+            $("#message").val('');
+
+            removeLoading();
+        }, 1000);
+
     } else{
-        $("#notification").append(`  
-        <div class="alert alert-danger alert-dismissible">
-            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            <strong>Error!</strong> Please fill in Name, Email, and Message
-        </div>`);
+        
+        setTimeout(function(){
+            $("#notification").append(`  
+            <div class="alert alert-danger alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Error!</strong> Please fill in Name, Email, and Message
+            </div>`);
+
+            removeLoading();
+        }, 1000);
     }
 
 });
+
+function removeLoading(){
+    $("#loading").html('');
+    $("#loading").remove();
+}
+
